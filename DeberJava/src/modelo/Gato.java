4f894570acb2c8c92084760ca800d1eb8ef1a2a3
oscar100dav <oscar.vargas877@ist17dejulio.edu.ec>
2025-10-18 16:22:40 -0500

    Primer ejercicio en Java Gato

---
+++ DeberJava/src/modelo/Gato.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+/**
+ *
+ * @author Usuario
+ */
+public class Gato {
+    // atributos
+    public String nombre;
+    public int edad;
+    
+    //metodo
+    public void maullar(){
+        System.out.println(nombre + " dice miau miau");
+    }
+// metodos 
+    public void mostrarInformacion(){
+        System.out.println("Gato: "  + nombre + " Edad: " + edad + " años");
+    }
+}
